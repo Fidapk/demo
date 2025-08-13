d9addb15b67d6ac862445e135cc98751d9b978e0
Fidapk <fidapk269@gmail.com>
2025-08-13 16:14:46 +0530

    Update high order function.js

--- high order function.js
+++ high order function.js
@@ -1,11 +1,14 @@
 function calculate(a,b,operation){
     return operation(a,b)
 }
+// addition
 function add(x,y){
     return x+y;
 }
+// subtraction
 function subtract (x,y){
     return x-y
 }
+// higher oder function
 console.log(calculate(10, 5, add));
 console.log(calculate(10, 5, subtract));
